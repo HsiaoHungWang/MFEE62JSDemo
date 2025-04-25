785d1a0407e7b86a033b8f80b8d9557cf31ee49a
HsiaoHungWang <wang@ispan.com.tw>
2025-04-25 14:23:47 +0800

    function的建立及使用

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,1 +1,8 @@
-document.write("Hello JavaScript");
+function sayHi(name){
+  //name => undefined => !false=> true
+  //name => Jack => !true => false
+  if(!name){
+     name = "Guest";
+  }
+  return `Hello ${name}`
+}
